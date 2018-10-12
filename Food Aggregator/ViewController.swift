c05a78ffd62492b565dee868fda7c1e9e3aaa0e3
Chashmeet Singh <chashmeetsingh@gmail.com>
2018-10-12 16:37:25 -0400

    Initial Commit

---
+++ Food Aggregator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Food Aggregator
+//
+//  Created by Chashmeet on 12/10/18.
+//  Copyright © 2018 Chashmeet Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
